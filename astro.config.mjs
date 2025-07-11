aadf989f625a8eacfa27fc1fa761fd78ee56eb57
maximeconceicao <maxime.conceicao@gmail.com>
2025-07-11 20:19:17 +0200

    feat: add gh-pages and start cleaning imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://maximeconceicao.github.io",
   base: "/temple-of-reflection/",
   vite: {
     plugins: [tailwindcss()],
